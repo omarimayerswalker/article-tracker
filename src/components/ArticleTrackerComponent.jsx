8b3c5a12be27065839920763023616e7f75e06f5
Omari Mayers-Walker <omari@fastmail.fm>
2016-02-18 20:53:52 -0500

    added articletrackercomponent

---
+++ src/components/ArticleTrackerComponent.jsx
@@ -0,0 +1,42 @@
+var React = require('react');
+
+var ArticleTrackerComponent = React.createClass({
+
+  getInitialState: function() {
+    return {data: []};
+  },
+
+  loadArticlesFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      datatype: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+
+  componentDidMount: function() {
+    this.loadArticlesFromServer();
+  },
+
+  render: function() {
+    return (
+      <div className="articleTracker">
+        <h1>Article Tracker</h1>
+        <h2>Article URL</h2>
+        <form className="articleForm">
+          <input type="text"></input>
+        </form>
+        <ArticleInputFormComponent/>
+        <ArticleListComponent data={this.state.data}/>
+      </div>
+    );
+  }
+});
+
+module.exports = ArticleTrackerComponent;
