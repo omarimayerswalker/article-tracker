fb5c95ff39d30885108f097437135d581dfa181c
Omari Mayers-Walker <omari@fastmail.fm>
2016-02-25 17:29:15 -0500

    renamed www to www.js

--- src/components/ArticleTrackerComponent.jsx
+++ src/components/ArticleTrackerComponent.jsx
@@ -2,8 +2,6 @@ var React = require('react');
 var ArticleListComponent = require('./ArticleListComponent.jsx');
 var ArticleInputFormComponent = require('./ArticleInputFormComponent.jsx');
 
-/* var ArticleTrackerActions = require('../actions/ArticleTrackerActions.jsx'); */
-
 var ArticleTrackerComponent = React.createClass({
 
   getInitialState: function() {
